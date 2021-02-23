3fef965b6b3b94c5c16fd8bee2b6e1eac6dbf5ee
JimHuertas <live.land18@gmail.com>
2021-02-23 07:51:07 -0500

    Commit inicial

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/widgets/card_swiper.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Peliculas en cines'),
+          backgroundColor: Colors.indigoAccent,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              _swiperTarjetas(),
+            ],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() => CardSwiper(
+        peliculas: [1, 2, 3, 4, 5],
+      );
+}
